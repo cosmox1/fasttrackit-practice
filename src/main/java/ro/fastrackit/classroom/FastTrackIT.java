0ebeaf17ef30fa3e72924ac10b41ab3c7e0c5a90
Cosmin <cosmin2323l9@gmail.com>
2020-10-13 21:51:22 +0300

    tema1

--- src/main/java/ro/fastrackit/classroom/FastTrackIT.java
+++ src/main/java/ro/fastrackit/classroom/FastTrackIT.java
@@ -3,19 +3,19 @@ package ro.fastrackit.classroom;
 public class FastTrackIT {
     public static void main(String[] args) {
 //atribuim valori obiectului student A ce trece prin constructorul de aceeasi forma
-     Student A = new Student ("Cosmin", "Panaite", 24);
+     Student studentA = new Student ("Cosmin", "Panaite", 24);
 //printam prin constructorul respectiv
-     System.out.println(A);
+     System.out.println(studentA);
      // setam noi numele prin setter
-        A.setFname("Mihnea");
-        A.setLname("Fasttrack");
-        A.setage(30);
+        studentA.setFname("Mihnea");
+        studentA.setLname("Fasttrack");
+        studentA.setage(30);
         //printam informatia inserata prin setters
-        System.out.println(A);
+        System.out.println(studentA);
 
-        Profesor B= new Profesor("Mihnea","Fasttrack",24,null);
+        Profesor profesorB= new Profesor("Mihnea","Fasttrack",24,true);
         //printam constructorul din profesor
-        System.out.println(B);
+        System.out.println(profesorB);
 PhdProfesor C=new PhdProfesor("Florin","Track",24, null);
 System.out.println(C);
 
--- src/main/java/ro/fastrackit/classroom/PhdProfesor.java
+++ src/main/java/ro/fastrackit/classroom/PhdProfesor.java
@@ -1,12 +1,7 @@
 package ro.fastrackit.classroom;
 
 public class PhdProfesor extends Profesor{
-    private Integer age;
-    private String Fname;
-    private String Lanme;
-    private Integer dateOfHiring;
-    private String email;
-    private Boolean phd;
+
     PhdProfesor (String Fname,String Lname, Integer age, Boolean phd){
         super( Fname,Lname, age, phd);
     }
